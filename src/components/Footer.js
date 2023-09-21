331a9b35ce362036a45e4429a1f5c8d87abc2b5b
Musaab Elsheikh <musaab@Musaabs-MacBook-Air.local>
2023-09-21 19:06:36 -0400

    voltwise frontend changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="footer">
       <div className="content has-text-centered">
         <p>
-           Hexal 2019. The source code is licensed MIT. The website content is licensed CC BY NC SA 4.0.
+           VoltWise 2019. The source code is licensed MIT. The website content is licensed CC BY NC SA 4.0.
         </p>
       </div>
     </footer>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bulma/css/bulma.min.css';
+import './custom.scss';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
